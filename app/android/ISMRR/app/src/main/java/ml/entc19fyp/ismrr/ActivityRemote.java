23ba9ba7c54b51a48534d345d2977c41c0d7ca5e
Chamod <abeywickramakcs.19@uom.lk>
2024-02-02 19:47:46 +0530

    change ui event structure

--- app/android/ISMRR/app/src/main/java/ml/entc19fyp/ismrr/ActivityRemote.java
+++ app/android/ISMRR/app/src/main/java/ml/entc19fyp/ismrr/ActivityRemote.java
@@ -15,20 +15,62 @@ import com.google.firebase.database.FirebaseDatabase;
 public class ActivityRemote extends AppCompatActivity {
 
     DatabaseReference dbRef;
-    ImageButton btnFront,btnBack,btnLeft,btnRight,btnStop,btnPlay;
+    ImageButton btnFront, btnBack, btnLeft, btnRight, btnStop, btnPlay;
 
-    View.OnTouchListener btnTouchListener =new View.OnTouchListener() {
+    ActDir actDir;
+
+    View.OnTouchListener btnTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View v, MotionEvent event) {
-            String topic=v.getTag().toString();
+            String tag = v.getTag().toString();
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    sendTopic(topic+"_prs");
+                    switch (tag) {
+                        case "F":
+                            actDir.F = 1;
+                            break;
+                        case "B":
+                            actDir.B = 1;
+                            break;
+                        case "L":
+                            actDir.L = 1;
+                            break;
+                        case "R":
+                            actDir.R = 1;
+                            break;
+                        case "S":
+                            actDir.S = 1;
+                            break;
+                        case "P":
+                            actDir.P = 1;
+                            break;
+                    }
+                    sendTopic();
                     v.setPressed(true);
                     v.performClick();
                     break;
                 case MotionEvent.ACTION_UP:
-                    sendTopic(topic+"_rls");
+                    switch (tag) {
+                        case "F":
+                            actDir.F = 0;
+                            break;
+                        case "B":
+                            actDir.B = 0;
+                            break;
+                        case "L":
+                            actDir.L = 0;
+                            break;
+                        case "R":
+                            actDir.R = 0;
+                            break;
+                        case "S":
+                            actDir.S = 0;
+                            break;
+                        case "P":
+                            actDir.P = 0;
+                            break;
+                    }
+                    sendTopic();
                     v.setPressed(false);
                     break;
                 // Handle other touch events here
@@ -36,19 +78,22 @@ public class ActivityRemote extends AppCompatActivity {
             return true; // Return true to indicate that the event has been handled
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote);
-        dbRef= FirebaseDatabase.getInstance().getReference("/ISMRR/robot/move");
-        Log.d("QWE","ST");
+        dbRef = FirebaseDatabase.getInstance().getReference("/ISMRR/robot/move");
+        Log.d("QWE", "ST");
 
-        btnFront=findViewById(R.id.btnFront);
-        btnBack=findViewById(R.id.btnBack);
-        btnLeft=findViewById(R.id.btnLeft);
-        btnRight=findViewById(R.id.btnRight);
-        btnStop=findViewById(R.id.btnStop);
-        btnPlay=findViewById(R.id.btnPlay);
+        actDir = new ActDir();
+
+        btnFront = findViewById(R.id.btnFront);
+        btnBack = findViewById(R.id.btnBack);
+        btnLeft = findViewById(R.id.btnLeft);
+        btnRight = findViewById(R.id.btnRight);
+        btnStop = findViewById(R.id.btnStop);
+        btnPlay = findViewById(R.id.btnPlay);
 
         btnFront.setOnTouchListener(btnTouchListener);
         btnBack.setOnTouchListener(btnTouchListener);
@@ -56,14 +101,22 @@ public class ActivityRemote extends AppCompatActivity {
         btnRight.setOnTouchListener(btnTouchListener);
         btnStop.setOnTouchListener(btnTouchListener);
         btnPlay.setOnTouchListener(btnTouchListener);
-
-
-
     }
 
-    public void sendTopic(String topic){
-        long timeStamp=System.currentTimeMillis();
-        dbRef.child(topic).setValue(timeStamp);
-        Log.d("QWE",topic+":"+timeStamp);
+    public void sendTopic() {
+//        long timeStamp = System.currentTimeMillis();
+//        dbRef.child(topic).setValue(timeStamp);
+//        Log.d("QWE", topic + ":" + timeStamp);
+        dbRef.setValue(actDir);
+    }
+
+    public class ActDir {
+        public int F = 0;
+        public int B = 0;
+        public int L = 0;
+        public int R = 0;
+        public int S = 0;
+        public int P = 0;
+
     }
 }
