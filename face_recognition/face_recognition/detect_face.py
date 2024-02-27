25f01e1bb1141a3083ba5d87ba932cf015cef39c
Vidura Munasinghe <81399281+vidurawarna@users.noreply.github.com>
2024-02-27 10:51:12 +0530

    working fr several people

--- face_recognition/face_recognition/detect_face.py
+++ face_recognition/face_recognition/detect_face.py
@@ -6,9 +6,9 @@ import time
 face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 camera = cv.VideoCapture(0)
 
-def main():
+# def main():
 
-    DeepFace.stream(db_path = "/home/vwm/fyp_ws/src/face_recognition/images",model_name="ArcFace",enable_face_analysis=False)
+#     DeepFace.stream(db_path = "/home/vwm/fyp_ws/src/face_recognition/images",model_name="ArcFace",enable_face_analysis=False)
 
 
     
@@ -35,20 +35,36 @@ def captureCam():
             break
 
 def detect_bounding_box(vid):
-    gray_image = cv.cvtColor(vid, cv.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
-    for (x, y, w, h) in faces:
-        cv.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
-        person = DeepFace.find(vid[x-40:x+w+40][y-40:y+h+40],db_path="/home/vwm/fyp_ws/src/face_recognition/images",model_name="ArcFace",enforce_detection=False)
+
+    people = DeepFace.find(vid,db_path="/home/vwm/fyp_ws/src/face_recognition/images",model_name="ArcFace",enforce_detection=False,threshold=0.5)
         
-        if(len(person)==0):
-            print("Unknown")
-        else:
-            person_name = person[0]['identity'][0].split('/')[8]
+        
+    if(not(people[0].empty)):
+
+        # print(people)
+        for i in range(len(people)):
+            person = people[i]
+            person_name = person['identity'][0].split('/')[8]
+            # print(str(i)+" "+person_name)
+
+            x = person['source_x'][0]
+            y = person['source_y'][0]
+            w = person['source_w'][0]
+            h = person['source_h'][0]
+
+            # print(x,y,w,h)
+
+            cv.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
             cv.putText(vid, person_name, (x,y+h+20), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2, cv.LINE_AA) 
+# 
+            # for h in person['identity']:
+            #     print(h.split('/')[8])
+        # else:
+        #     person_name = person[0]['identity'][0].split('/')[8]
+        #     cv.putText(vid, person_name, (x,y+h+20), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2, cv.LINE_AA) 
         # print(person_path[8])
 
-    return faces
+    return 1
 
 if __name__ == '__main__':
     captureCam()
