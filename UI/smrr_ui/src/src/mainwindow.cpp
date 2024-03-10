a4b601cfe2e541acd5553a6feb2563ec1ee0c616
Chamod <abeywickramakcs.19@uom.lk>
2024-03-10 16:28:16 +0530

    run python sciprt from button click

--- UI/smrr_ui/src/src/mainwindow.cpp
+++ UI/smrr_ui/src/src/mainwindow.cpp
@@ -14,29 +14,59 @@
 #include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);    
-    commNode=new rclcomm();
-    connect(commNode,SIGNAL(emitTopicData(QString)),this,SLOT(updateTopicInfo(QString)));
-    connect(ui->pushButton,&QPushButton::clicked,commNode,&rclcomm::sendTopicData);
+    ui->setupUi(this);
+    commNode = new rclcomm();
+    connect(commNode, SIGNAL(emitTopicData(QString)), this, SLOT(updateTopicInfo(QString)));
+    connect(ui->pushButton, &QPushButton::clicked, commNode, &rclcomm::sendTopicData);
     qDebug() << "aaa run";
-
 }
-void MainWindow::updateTopicInfo(QString data){
+void MainWindow::updateTopicInfo(QString data)
+{
     ui->label->clear();
     ui->label->setText(data);
 }
 
 void MainWindow::on_pushButton2_clicked()
 {
-    if (!QProcess::startDetached("code"))
-        QMessageBox::information(this, "Code run", "Code run by button");
+    QProcess *process = new QProcess(this);
+
+    // Set the absolute path to the Python script
+    QString scriptPath = "/home/csa/fyp/UI/smrr_ui/test/long_running_script.py";
+
+    // Set the command to be executed (Python interpreter with the absolute path to the script as argument)
+    QString command = "python3";
+    QStringList arguments;
+    arguments << scriptPath;
+
+    // Start the process with the command
+    process->start(command, arguments);
+
+    // Connect process signals to slots
+    QObject::connect(process, &QProcess::readyReadStandardOutput, [=]()
+                     {
+        // Read the standard output of the process
+        
+        QString output = process->readAllStandardOutput();
+        qDebug() << output;
+        // Append the output to the label
+        ui->label->setText(ui->label->text() + "\n" + output); });
+
+    connect(process, static_cast<void (QProcess::*)(int)>(&QProcess::finished),
+
+            [=](int exitCode)
+            {
+                process->deleteLater();
+                qDebug() << exitCode;
+            }
+
+    );
+
+    
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
-
---
+++ UI/smrr_ui/test/long_running_script.py
@@ -0,0 +1,9 @@
+import time
+
+print("hello world", flush=True)
+# Simulate a long-running task
+for i in range(3):
+    print(f"Output {i}", flush=True)
+    time.sleep(1)
+
+print("hello", flush=True)
