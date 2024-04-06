4d32bda83cc09f9ff1f9ab2f56ed4723f78fb1af
Chamod <abeywickramakcs.19@uom.lk>
2024-04-06 23:20:42 +0530

    generate screen info

---
+++ UI/smrr_ui/src/include/screen_info.h
@@ -0,0 +1,33 @@
+#ifndef SCREEN_INFO_H
+#define SCREEN_INFO_H
+
+#include <QDialog>
+#include <QString>
+
+// #include <string>
+// #include <vector>
+
+// #include "rclcomm.h"
+// #include "option.h"
+
+#include "mainwindow.h"
+
+using namespace std;
+
+namespace Ui {
+class ScreenInfo;
+}
+
+class ScreenInfo : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ScreenInfo(QWidget *parent = nullptr /*TODO: , vector<Option> *options=nullptr, QString text="", PubStr *pubStr = nullptr*, string data="" */);
+    ~ScreenInfo();
+
+private:
+    Ui::ScreenInfo *ui;
+};
+
+#endif // SCREEN_INFO_H
