8f4f89796ca3910d5413ccaeaeb379714e140c8b
Chamod <abeywickramakcs.19@uom.lk>
2024-04-07 00:12:50 +0530

    generate screen name

---
+++ UI/smrr_ui/src/include/screen_name.h
@@ -0,0 +1,33 @@
+#ifndef SCREEN_NAME_H
+#define SCREEN_NAME_H
+
+#include <QDialog>
+#include <QString>
+
+// #include <string>
+// #include <vector>
+
+// #include "rclcomm.h"
+// #include "option.h"
+
+#include "mainwindow.h"
+
+using namespace std;
+
+namespace Ui {
+class ScreenName;
+}
+
+class ScreenName : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit ScreenName(QWidget *parent = nullptr /*TODO: , vector<Option> *options=nullptr, QString text="", PubStr *pubStr = nullptr*, string data="" */);
+    ~ScreenName();
+
+private:
+    Ui::ScreenName *ui;
+};
+
+#endif // SCREEN_NAME_H
