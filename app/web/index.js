8fb26b41af79fa385ebdda3076fef9cd5b8013e9
Chamod <abeywickramakcs.19@uom.lk>
2024-04-08 22:27:56 +0530

    add web interface

--- app/web/index.js
+++ app/web/index.js
@@ -13,15 +13,43 @@ firebase.initializeApp(firebaseConfig);
 
 // Reference to your Firebase Realtime Database
 const database = firebase.database();
-const dbRef = database.ref('/ISMRR/cmd/move'); // Replace with your database path
+const dbRef = database.ref('/ISMRR/'); // Replace with your database path
 
 // Function to read the value from the database
-dbRef.on('value', (snapshot) => {
-    const value = snapshot.val();
-    document.getElementById('dbValue').textContent = value;
+dbRef.on('value', (snapshot) => {    
+    const str = displayObject(snapshot.val());
+    console.log(str);
+    document.getElementById('dbValue').innerHTML = `<pre>${str}</pre>`;
 });
 
 // Function to update the value in the database
 function sendTopic(topic) {    
     dbRef.child(topic).set(Date.now());    
+}
+
+function displayObject(obj, indent = 0) {
+    let result = '';
+
+    // Helper function to generate indentation
+    const generateIndent = (count) => '  '.repeat(count);
+
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const value = obj[key];
+
+            // Add current key-value pair to result string with proper indentation
+            result += generateIndent(indent) + key + ': ';
+
+            if (typeof value === 'object' && value !== null) {
+                // If the value is an object, recursively call displayObject with increased indentation
+                result += '\n' + displayObject(value, indent + 1)+'\n';                
+            } else {
+                // If the value is not an object, append it to the result string
+                result += value + '\n';
+            }
+            
+        }
+    }
+
+    return result;
 }
