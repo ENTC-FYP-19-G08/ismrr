62dc9bc4553c090135dadbf926a9a8525ab7a0d5
Chamod <abeywickramakcs.19@uom.lk>
2024-04-17 12:15:03 +0530

    load image in map

--- UI/smrr_ui/src/src/screen_map.cpp
+++ UI/smrr_ui/src/src/screen_map.cpp
@@ -3,8 +3,9 @@
 #include "./ui_screen_map.h"
 
 #include <QDebug>
-
-ScreenMap::ScreenMap(QWidget *parent ,QString text, string data)
+#include <QPixmap>
+#include <QLabel>
+ScreenMap::ScreenMap(QWidget *parent, QString text, string data)
     : QDialog(parent), ui(new Ui::ScreenMap)
 {
     ui->setupUi(this);
@@ -13,15 +14,28 @@ ScreenMap::ScreenMap(QWidget *parent ,QString text, string data)
 
     qDebug() << "Map window loaded";
 
+    QPixmap pixmap("imgs/img1.jpg");
+    if (pixmap.isNull())
+    {
+        // Handle error if image loading fails (e.g., display a message)
+        qDebug() << "Error loading image";
+    }
+    else
+    {
+        QPixmap scaledPixmap = pixmap.scaled(ui->labelImg->size(), Qt::KeepAspectRatioByExpanding, Qt::SmoothTransformation);
+        // QPixmap scaledPixmap = pixmap.scaled(ui->labelImg->size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+        ui->labelImg->setPixmap(scaledPixmap);
+    }
+
     ui->label->setText(text);
 
     std_msgs::msg::String rosString;
     rosString.data = data;
-    mainWindow->rosNode->pubGuideVerbal->publish(rosString);   
+    mainWindow->rosNode->pubGuideVerbal->publish(rosString);
 
     connect(ui->btnBack, &QPushButton::clicked, mainWindow, &MainWindow::btnBack_clicked);
     connect(ui->btnHome, &QPushButton::clicked, mainWindow, &MainWindow::btnHome_clicked);
-
 }
 
 ScreenMap::~ScreenMap()
